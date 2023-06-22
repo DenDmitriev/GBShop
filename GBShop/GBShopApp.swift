8dd58d34fa01c83a6cfb4591bc3cdbcbe510eca2
DenDmitriev <dv.denstr@gmail.com>
2023-06-22 09:38:55 +0300

    first init

---
+++ GBShop/GBShopApp.swift
@@ -0,0 +1,20 @@
+//
+//  GBShopApp.swift
+//  GBShop
+//
+//  Created by Denis Dmitriev on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GBShopApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
