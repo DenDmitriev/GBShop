0f60d00b09561d023825a49560fa6b952aab673c
DenDmitriev <dv.denstr@gmail.com>
2023-06-23 09:51:01 +0300

    add network core

---
+++ GBShop/Core/Network/ErrorParser.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorParser.swift
+//  GBShop
+//
+//  Created by Denis Dmitriev on 23.06.2023.
+//
+
+import Foundation
+
+class ErrorParser: AbstractErrorParser { func parse(_ result: Error) -> Error {
+    return result }
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+        return error
+    }
+}
