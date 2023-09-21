4e847867544a5c29f5601b260c2fccef04aa58f5
DenDmitriev <dv.denstr@gmail.com>
2023-09-21 14:23:54 +0300

    add firebase for analytics

--- GBShop/App/GBShopApp.swift
+++ GBShop/App/GBShopApp.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// swiftlint:disable line_length
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+// swiftlint:enable line_length
 
 @main
 struct GBShopApp: App {
 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @ObservedObject var viewModel: GBShopAppViewModel
 
     init() {
