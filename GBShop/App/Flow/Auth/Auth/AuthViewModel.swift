4bba211ab30e4692d018ab2730b991eb9912d800
DenDmitriev <dv.denstr@gmail.com>
2023-10-10 15:34:10 +0300

    fix error

--- GBShop/App/Flow/Auth/Auth/AuthViewModel.swift
+++ GBShop/App/Flow/Auth/Auth/AuthViewModel.swift
@@ -24,7 +24,7 @@ class AuthViewModel: ObservableObject {
     func loginNew(login: String, password: String) async {
         let requestModel = AuthRequest.LoginUser(
             baseUrl: URL(string: "baseUrl")!,
-            headers: [.authorization(username: login, password: password)],
+            headers: [.authorization(username: login.lowercased(), password: password)],
             email: login.lowercased(),
             password: password
         )
